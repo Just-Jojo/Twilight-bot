618c9dfb81650ec6c957a21b77e611015441abb2
Jojo <Jojo@gmail.com>
2020-08-01 13:04:19 -0400

    Added a list instead of reading a file.

--- cogs/mylittlepony.py
+++ cogs/mylittlepony.py
@@ -59,12 +59,20 @@ class MyLittlePony(Cog, name="mylittlepony"):
 
     @com.command(name="quote", aliases=["twq", "twilight"], help="Get a random Twilight quote")
     async def twilight_quotes(self, ctx):
-        x = []
-        with open("twilightquote.txt", "r") as f:
-            for line in f.readlines():
-                x.append(line)
+        quotes = [
+            "All the ponies in this town are crazy! Do you know what time it is?!",
+            "You see, Nightmare Moon, when those Elements are ignited by the... the spark, that resides in the heart of us all, it creates the sixth element: the element of... magic!",
+            "Hee-hee! Isn't this exciting? We'll do everything by the book, and that will make my slumber party officially fun.",
+            "Everypony everywhere has a special magical connection with her friends, maybe even before she's met them. If you're feeling lonely and you're still searching for your true friends, just look up in the sky. Who knows, maybe you and your future best friends are all looking at the same rainbow.",
+            "Move! Look out, here comes Tom!",
+            "Clock is ticking, Twilight. Clock. Is. Ticking. Keep it together. If I can't find a friendship problem... I'll make a friendship problem!",
+            "Gee, maybe her name should be Princess Demandy-pants.",
+            "Huh? I'm pancake...I mean awake!",
+            "Soup spoon, salad fork, pasta spoon, strawberry pick, I'm beginning to think that after friendship, the greatest magic of all is proper silverware placement!",
+            "As the Princess of Friendship, I try to set an example for all of Equestria. But today, it was Spike who taught me that a new friend can come from anywhere. I guess everypony still has things to learn about friendship. Even me! And if Spike says Thorax is his friend, then he's my friend too."
+        ]
         embed = discord.Embed(color=discord.Color.purple(
-        ), title="Twilight quote", description=x[random.randrange(0, len(x))])
+        ), title="Twilight quote", description=quotes[random.randrange(0, len(quotes))])
         embed.set_thumbnail(
             url=twilight_image_links[random.randrange(
                 0, len(twilight_image_links))]
