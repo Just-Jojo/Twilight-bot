1832fd0a394461d0418a09129779f109e38effd6
Just-Jojo <jojohelp39@gmail.com>
2020-11-17 18:24:21 -0500

    Smaller updates including an info and license command

--- lib/cogs/core.py
+++ lib/cogs/core.py
@@ -1,5 +1,29 @@
+"""
+MIT License
+
+Copyright (c) 2020 Jojo#7711
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+
 ### ~~~ Basic Discord and other utilities imports ~~~ ###
 import discord
+from discord import __version__ as dpyversion
 from discord.ext import commands
 from discord.ext.commands import (
     command, Cog, is_owner, Context, check, group
@@ -70,11 +94,11 @@ class Core(Cog):
 
     @Cog.listener()
     async def on_guild_join(self, guild: discord.Guild):
-        Moderation.setup(guild)
+        Moderation.setup(self, guild)
 
     @Cog.listener()
     async def on_guild_leave(self, guild: discord.Guild):
-        Moderation.teardown(guild)
+        Moderation.teardown(self, guild)
 
     @command()
     @is_owner()
@@ -85,6 +109,27 @@ class Core(Cog):
         if len(self.bot.last_exception) > 1990:
             return await ctx.send("I can't send the traceback as it's over 2000 characters long")
         await ctx.send(self.bot.last_exception)
+
+    @command()
+    async def info(self, ctx: Context):
+        embed = Embed.create(
+            self, ctx, title="Twilight bot Info", description="Version informations"
+        )
+        embed.add_field(
+            name="discord.py version", value=dpyversion, inline=False
+        )
+        embed.add_field(
+            name="Twilight bot version", value=self.bot.version, inline=False
+        )
+        await ctx.send(embed=embed)
+
+    @command(name="license")
+    async def _license(self, ctx):
+        embed = Embed.create(
+            self, ctx, title="Twilight bot License",
+            description=self.bot.license, footer="Twilight bot License :D"
+        )
+        await ctx.send(embed=embed)
 
     @Cog.listener()
     async def on_ready(self):
