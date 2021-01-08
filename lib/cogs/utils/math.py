353196c9d567393509c5661588915f915b493672
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-08 18:28:14 +0000

    Added support for fraction->decimal converters

--- lib/cogs/utils/math.py
+++ lib/cogs/utils/math.py
@@ -2,8 +2,19 @@ import discord
 
 def fraction(number: float):
   """
-  Returns a fraction for the float specified.
+  Convert an integer/float to a fraction.
   """
   n = number
   return(f"{(n).as_integer_ratio()[0]}/{(n).as_integer_ratio()[1]}")
-  # Yes, kreusada did actually figure this one :D
+
+def decimal(fraction: str):
+  """
+  Convert a fraction to a decimal.
+  Must be in the format `int/int`.
+  """
+  f = fraction
+  l, r = f.split('/')
+  if l.isdigit() and r.isdigit() and '/' in f:
+    return(f"{float(l)/float(r)}")
+  else:
+    return("Invalid fraction")
