1a0534c4668b1d4a95ab7c53bd2fe322516b6bcf
Just Jojo <justjojoGH@gmail.com>
2021-01-21 13:28:59 -0500

    Version bump 0.1.8

--- bot/bot.py
+++ bot/bot.py
@@ -123,7 +123,7 @@ class Twilight(BotBase):
         key `users` contains the blocklisted user ids
         key `guilds` contains the blocklisted guild ids
     """
-    __version__ = "0.1.7"
+    __version__ = "0.1.8"
     __author__ = "Jojo#7791"
 
     def __init__(self):
