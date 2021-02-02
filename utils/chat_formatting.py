ead5bb74b950a01a19321857d31744f6e3f55830
kreusada <67752638+kreus7@users.noreply.github.com>
2021-02-02 14:02:08 +0000

    [chat_formatting] Type hinting

--- utils/chat_formatting.py
+++ utils/chat_formatting.py
@@ -1,35 +1,35 @@
-def bold(text):
+def bold(text: str):
   """Returns a string with boldened characters."""
   return "**{}**".format(text)
   
-def italic(text):
+def italic(text: str):
   """Returns a string with italicized characters."""
   return "*{}*".format(text)
   
-def strike(text):
+def strike(text: str):
   """Returns a string with striked characters."""
   return "~~{}~~".format(text)
   
-def spoiler(text):
+def spoiler(text: str):
   """Returns a string with hidden characters."""
   return "||{}||".format(text)
   
-def quote(text):
+def quote(text: str):
   """Returns a quoted string."""
   return ">>> {}".format(text)
   
-def backtick(text):
+def backtick(text: str):
   """Returns a string with backticks"""
   return "`{}`".format(text)
   
-def box(text, lang):
+def box(text: str, lang: str = ''):
   """Returns a string inside a code-block."""
   return "```{}\n{}\n```.format(lang, text)
   
-def snake(text):
+def snake(text: str):
   """Returns a string with snake spaces."""
   return text.replace(' ', '_')
 
-def reverse(text):
+def reverse(text: str):
   """Returns a reversed string."""
   return text[::-1]
