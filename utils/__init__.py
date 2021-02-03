539a6a858abc5221d57f4bf97464958e65e7c514
Just Jojo <justjojoGH@gmail.com>
2021-02-03 14:15:45 -0500

    [Utils] Imports, Licensing, and fixed a missing quote issue

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,13 @@
 from .basic_utils import administrator as admin
-from .basic_utils import (box, edit_guild_settings, get_guild_settings,
-                          get_settings, guild_setup, humanize_timedelta,
-                          is_admin, is_mod)
+from .basic_utils import (
+    edit_guild_settings, get_guild_settings,
+    get_settings, guild_setup, humanize_timedelta,
+    is_admin, is_mod
+)
 from .basic_utils import moderator as mod
 from .basic_utils import teardown, tick, write_settings
 from .character_parser import char_embed
+from .chat_formatting import *
 from .converter import RawUserIds
 from .embed import Embed
 from .math import decimal, fraction
